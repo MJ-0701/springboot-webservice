61d69e0dbdd8e2cc43419b50f8b173d0931170be
채명정 <jack2718@naver.com>
2021-03-29 03:11:19 +0900

    0329 03:10AM 로그인 세션정보값 어노테이션화

--- src/main/java/com/example/springbootwebservice/web/IndexController.java
+++ src/main/java/com/example/springbootwebservice/web/IndexController.java
@@ -1,6 +1,7 @@
 package com.example.springbootwebservice.web;
 
 
+import com.example.springbootwebservice.config.auth.LoginUser;
 import com.example.springbootwebservice.config.auth.dto.SessionUser;
 import com.example.springbootwebservice.domain.user.User;
 import com.example.springbootwebservice.service.posts.PostsService;
@@ -21,15 +22,15 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
 
         model.addAttribute("posts",postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-
-        if(user != null){
+        if (user != null){
             model.addAttribute("userName", user.getName());
         }
+
+
 
         return "index";
     }
