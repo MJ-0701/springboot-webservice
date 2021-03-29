f034bba75dacbf2b96d0796f6cdfc9896aa1643f
채명정 <jack2718@naver.com>
2021-03-29 23:05:51 +0900

    0329 11:05PM 세션정보값 디비저장 설정

---
+++ src/main/java/com/example/springbootwebservice/domain/session/SpringSession.java
@@ -0,0 +1,38 @@
+package com.example.springbootwebservice.domain.session;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@NoArgsConstructor
+@Getter
+@Table(name = "spring_session")
+@Entity
+public class SpringSession {
+
+    @Id
+    @Column(name = "primary_id")
+    private String primaryId;
+
+    @Column(name = "session_id")
+    private String sessionId;
+
+    @Column(name = "creatian_time")
+    private String creationTime;
+
+    @Column(name = "last_access_time")
+    private String lastAccessTime;
+
+    @Column(name = "max_inactive_interval")
+    private String maxInactiveInterval;
+
+    @Column(name = "expiry_time")
+    private String expiryTime;
+
+    @Column(name = "principal_name")
+    private String principalName;
+}
