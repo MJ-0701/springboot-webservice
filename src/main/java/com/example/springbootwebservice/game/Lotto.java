81b70700f9982e82e2040f3e0e24e71e3321a9d6
채명정 <jack2718@naver.com>
2022-04-20 22:18:15 +0900

    참고용

--- src/main/java/com/example/springbootwebservice/game/Lotto.java
+++ src/main/java/com/example/springbootwebservice/game/Lotto.java
@@ -7,11 +7,11 @@ import java.util.Random;
 public class Lotto {
 
     public String lotto(){
-         int[] lotto = new int[7];
+         int[] lotto = new int[6];
 
          Random random = new Random();
 
-        for(int i =0; i<7; i++){
+        for(int i =0; i<6; i++){
             lotto[i] = random.nextInt(45)+1;
             for(int j = 0; j<i; j++){
                 if(lotto[i] == lotto[j]){
